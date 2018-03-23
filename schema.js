b5794291515f686a0ff7ce92074752fda68568d4
SarahHale <id15she@cs.umu.se>
2018-03-23 11:34:06 +0100

    working on function

--- schema.js
+++ schema.js
@@ -7,17 +7,35 @@ const {
   GraphQLString,
   GraphQLList
 } = require('graphql')
-const url = 'http://data.riksdagen.se/personlista/?iid=&fnamn=&enamn=&f_ar=&kn=&parti=&valkrets=&org=&utformat=json'
 
 
+//TODO: nr from each parti
+
+const ledamotFromParti = async(nrOfLedamot, parti) => {
+  let url = `http://data.riksdagen.se/personlista/?iid=&fnamn=&enamn=&f_ar=&kn=&parti=${parti}&valkrets=&org=&utformat=json`
+
+  const response = await fetch(url);
+  const json = await response.json();
+  console.log(json)
+  const ledamoter = json.personlista.person
+
+  let randomLedamoter = []
+
+  for (i = 0; i < nrOfLedamot; i++) {
+    randomLedamoter.push(ledamoter[Math.floor(Math.random() * ledamoter.length)])
+  }
+  console.log(randomLedamoter)
+  return randomLedamoter;
+
+}
 const LedamotType = new GraphQLObjectType({
   name: 'Ledamot',
   fields: () => ({
     sourceid: { type: GraphQLString },
     efternamn: { type: GraphQLString },
     tilltalsnamn: { type: GraphQLString },
-    bild_url_max: {type: GraphQLString},
-    parti: {type: GraphQLString}
+    bild_url_max: { type: GraphQLString },
+    parti: { type: GraphQLString }
   })
 })
 
@@ -30,14 +48,13 @@ const RootQuery = new GraphQLObjectType({
         id: { type: GraphQLString }
       },
       resolve(_, args) {
-      //TODO: Get from unike id
+        //TODO: Get from unike id
       }
     },
     ledamoter: {
       type: new GraphQLList(LedamotType),
       resolve(_, args) {
-        return fetch(url).then(u => u.json()).then(
-          result => result.personlista.person).catch(err => console.log(err))
+        ledamotFromParti(3, "M").then(result => console.log(result))
 
       }
     }
